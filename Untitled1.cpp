a7cf4c43531d3810d3ab50ff681c7be10f1b21ea
firdiseptiady <firdis998@gmail.com>
2018-01-19 18:22:00 +0800

    pertama

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class sepatu {
+    public:
+        void jenissepatu(string sepatu);
+        string merksepatu;
+        void ukuransepatu(string ukuran);
+    
+                
+        };
