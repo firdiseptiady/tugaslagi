0940e6c834808601be25b56015484c6b46cfa1c1
firdiseptiady <firdis998@gmail.com>
2018-01-19 18:22:59 +0800

    kedua

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -10,3 +10,28 @@ class sepatu {
     
                 
         };
+        int main(){
+    sepatu boots,sneakers,loafers; 
+
+	boots.merksepatu	="nike ";
+	sneakers.merksepatu	="adidas ";
+	loafers.merksepatu  ="vans ";
+
+	
+	cout << "boots: \n\n";
+	cout << (boots.merksepatu);
+	boots.ukuransepatu("40 ");
+	cout <<"===================================\n\n";
+	
+	cout << "sneakers: \n\n";
+	cout << (sneakers.merksepatu);
+	sneakers.ukuransepatu("39 ");
+	cout <<"===================================\n\n";
+	
+	cout << "loafers: \n\n";
+	cout << (loafers.merksepatu);
+	loafers.ukuransepatu("43 ");
+	cout <<"===================================\n\n";
+	
+
+	}
